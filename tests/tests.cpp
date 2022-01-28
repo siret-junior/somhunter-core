0c6a6809aa0b9df078507bcf64b4b18ff3d1471c
skrhakv <v.skrhak@gmail.com>
2022-01-28 10:40:21 +0100

    Update licence headers

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,7 @@
  * Copyright (C) 2021 František Mejzlík <frankmejzlik@gmail.com>
  *                    Mirek Kratochvil <exa.exa@gmail.com>
  *                    Patrik Veselý <prtrikvesely@gmail.com>
+ *                    Vít Škrhák <v.skrhak@gmail.com>
  *
  * SOMHunter is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free
